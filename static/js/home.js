3982fb6aee56699e58ffdd006ef428f794ec4c71
githubbubber <mekesiabrown@gmail.com>
2018-12-23 03:02:54 -0500

    Initial commit of css, js, and images

---
+++ static/js/home.js
@@ -0,0 +1,31 @@
+/* $(document).ready(function() {
+	 function subscriptionPopup(){
+	    //  get the mPopup
+	     var mpopup = $('#mpopupBox');
+	
+	    //  open the mPopup
+	     mpopup.show();
+	
+	    //  close the mPopup once close element is clicked
+	     $(".close").on('click',function(){
+	         mpopup.hide();
+	     });
+	
+	    //  close the mPopup when user clicks outside of the box
+	     $(window).on('click', function(e) {
+	         if(e.target == mpopup[0]){
+	             mpopup.hide();
+	         }
+	     });
+	 }
+
+     var popDisplayed = $.cookie('popDisplayed');
+     if(popDisplayed == '1'){
+         return false;
+     }else{
+         setTimeout( function() {
+             subscriptionPopup();
+         },10000);
+         $.cookie('popDisplayed', '1', { expires: 7 });
+     }
+ });*/
